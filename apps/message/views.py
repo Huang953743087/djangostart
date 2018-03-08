d7be980a9d80026f5edd745af988678ba264504a
Zehao.Huang <953743087@qq.com>
2018-03-08 14:25:08 +0800

    简易留言板功能练习,进行存储修改

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,3 +1,28 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
+from message.models import UserMessage
+
+
+def getform(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        message = request.POST.get('massage', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+    # all_message = UserMessage.objects.filter(name='metianyan', address='河南')
+    # if all_message:
+    #     message = all_message[0]
+
+    # for message in all_message:
+    #     print(message.name)
+    user_message = UserMessage()
+    user_message.name = name
+    user_message.message = message
+    user_message.address = address
+    user_message.email = email
+    user_message.object_id = random.random(1, 1000)
+    user_message.save()
+    return render(request, 'message_form.html', {'my_message':message})
