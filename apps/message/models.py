5d3d4e8f58a3e95ea406f8eed06a53fe1c5dd0e8
Zehao.Huang <953743087@qq.com>
2018-04-09 11:09:20 +0800

    简易留言板功能练习,进行存储修改

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,17 @@
+#-*-coding:utf-8-*-
 from django.db import models
 
 # Create your models here.
+
+class UserMessage(models.Model):
+    #orm max_length最大长度,verbos_name应该是字段名
+    name = models.CharField(max_length=10, null=True, blank=True, verbose_name=u'用户名')
+    email = models.EmailField(verbose_name=u'邮箱')
+    address = models.CharField(max_length=30, verbose_name=u'地址')
+    message = models.CharField(max_length=200, verbose_name=u'信息')
+    object_id = models.CharField(primary_key=True, max_length=30, default='', verbose_name='主键')
+
+    class Meta:
+        verbose_name = u'用户留言信息'
+        ordering = ('-object_id',)
+        verbose_name_plural = u'用户留言信息'
--- djangostart/settings.py
+++ djangostart/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'message',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'djangostart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangotest',
+        'USER': 'root',
+        'PASSWORD': 'huang953743087',
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -119,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
